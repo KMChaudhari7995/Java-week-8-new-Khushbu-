1c4b9b726e91ab70c3edbc3a0e59bf6c9dedbe5c
KMchaudhari7995 <kmchaudhari7912@gmail.com>
2023-04-06 23:55:05 +0100

    My week 8 JavaHomework Khushbu

---
+++ src/programme_21_abstractexample/Circle.java
@@ -0,0 +1,12 @@
+package programme_21_abstractexample;
+
+/*
+Create a Third class name Circle1 which extends by Shape.
+ */
+public class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("drawing circle ");
+
+    }
+}
---
+++ src/programme_24_method_overriding/SBI.java
@@ -0,0 +1,8 @@
+package programme_24_method_overriding;
+/*
+Creating child class */
+public class SBI extends Bank {
+    public int getRateOfInterest(){
+        return 8;
+    }
+}
